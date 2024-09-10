42ab962041ffe5c18b18e31dc208a498f5d85d29
a.hrebiankov <a.hrebiankov@enova.by>
2024-09-11 01:30:57 +0300

    feat: Added repository Petient

---
+++ HealthMonitor.Domain/AggregatesModel/IPatientRepository.cs
@@ -0,0 +1,19 @@
+﻿using HealthMonitor.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthMonitor.Domain.AggregatesModel
+{
+    public interface IPatientRepository : IRepository<Patient>
+    {
+        Patient Add(Patient patient);
+        Patient Update(Patient patient);
+        Task<Patient> Get(int Id);
+        Task<IEnumerable<Patient>> GetAll(Expression<Func<Patient, bool>> predicate);
+        Task Delete(int Id);
+    }
+}
